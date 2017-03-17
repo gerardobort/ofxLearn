fb081dccd5fdb3530d07cd9afa8d614af0797124
Gerardo Bort <gbort@medallia.com>
2017-03-17 20:49:23 -0300

    making little changes to make it compile well using just make

--- addon_config.mk
+++ addon_config.mk
@@ -51,8 +51,10 @@ common:
 	# in the src folders in libs and the root of the addon. if your addon needs
 	# to include files in different places or a different set of files per platform
 	# they can be specified here
-	ADDON_SOURCES = src/ofxLearn.cpp
-	ADDON_SOURCES += src/ofxLearn.h
+	#ADDON_SOURCES = src/ofxLearn.cpp
+	#ADDON_SOURCES += src/ofxLearn.h
+	ADDON_SOURCES = $(OF_ROOT)/addons/ofxLearn/src/ofxLearn.cpp
+	ADDON_SOURCES += $(OF_ROOT)/addons/ofxLearn/src/ofxLearn.h
 	
 	# some addons need resources to be copied to the bin/data folder of the project
 	# specify here any files that need to be copied, you can use wildcards like * and ?
